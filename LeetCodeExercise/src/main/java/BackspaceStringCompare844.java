8c7e91a7464566d831f9799b13a5ca955da91538
Tom <pjoh1092@gmail.com>
2023-02-26 20:48:32 -0600

    844. Backspace String Compare

---
+++ LeetCodeExercise/src/main/java/BackspaceStringCompare844.java
@@ -0,0 +1,31 @@
+import java.util.Stack;
+
+public class BackspaceStringCompare844 {
+    public boolean backspaceCompare(String s, String t) {
+        Stack<Character> sStack = new Stack<>();
+        Stack<Character> tStack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++){
+            if (s.charAt(i) != '#'){
+                sStack.push(s.charAt(i));
+            } else {
+                if (!sStack.isEmpty()){
+                    sStack.pop();
+                }
+
+            }
+        }
+
+        for (int i = 0; i < t.length(); i++){
+            if (t.charAt(i) != '#'){
+                tStack.push(t.charAt(i));
+            } else {
+                if (!tStack.isEmpty()){
+                    tStack.pop();
+                }
+
+            }
+        }
+        return sStack.equals(tStack);
+    }
+}
