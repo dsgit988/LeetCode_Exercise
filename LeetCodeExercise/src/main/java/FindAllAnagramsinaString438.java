cb29ef0ab853509ac3a14e84d79db396a2cd46b4
Tom <pjoh1092@gmail.com>
2023-02-26 19:36:07 -0600

    438. Find All Anagrams in a String

---
+++ LeetCodeExercise/src/main/java/FindAllAnagramsinaString438.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FindAllAnagramsinaString438 {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> result = new ArrayList<>();
+
+        int length = p.length();
+
+        char[] pArr = p.toCharArray();
+        Arrays.sort(pArr);
+
+
+        for (int i = 0; i < s.length() - length + 1; i++){
+            char[] subArr = s.substring(i,i + length).toCharArray();
+
+            Arrays.sort(subArr);
+
+            if (Arrays.equals(pArr,subArr)){
+                result.add(i);
+            }
+        }
+        System.out.println(result);
+        return result;
+    }
+}
