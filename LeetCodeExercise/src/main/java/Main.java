ab982af23d07832236c6e87a31319467860e28c5
Tom <pjoh1092@gmail.com>
2023-03-12 19:26:14 -0500

    56. Merge Intervals

--- LeetCodeExercise/src/main/java/Main.java
+++ LeetCodeExercise/src/main/java/Main.java
@@ -2,8 +2,12 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        String s = "Let's take LeetCode contest";
-        ReverseWordsinaStringIII r = new ReverseWordsinaStringIII();
-        System.out.println(r.reverseWords(s));
+//        String s = "Let's take LeetCode contest";
+//        ReverseWordsinaStringIII r = new ReverseWordsinaStringIII();
+//        System.out.println(r.reverseWords(s));
+
+        MergeIntervals m = new MergeIntervals();
+        int[][] intervals = new int[][]{{1,3},{2,6},{8,10},{15,18}};
+        m.merge(intervals);
     }
 }
