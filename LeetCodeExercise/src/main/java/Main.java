0d26ddd0af9b92c2f52eeb0fed8049055336f786
Tom <pjoh1092@gmail.com>
2024-02-07 15:11:08 -0600

    RemoveDuplicatesfromSortedArray

--- LeetCodeExercise/src/main/java/Main.java
+++ LeetCodeExercise/src/main/java/Main.java
@@ -3,8 +3,10 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        ReverseVowelsofaString r = new ReverseVowelsofaString();
-        r.reverseVowels("leetcode");
+        RemoveDuplicatesfromSortedArray r = new RemoveDuplicatesfromSortedArray();
+        int[] arr = {0,0,1,1,1,2,2,3,3,4};
+        System.out.println(r.removeDuplicates(arr));
+
 
     }
 }
