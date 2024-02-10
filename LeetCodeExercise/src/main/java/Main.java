39f786a7475de85e397ad2baea9eb4c24962735c
Tom <pjoh1092@gmail.com>
2024-02-10 14:05:43 -0600

    remove element

--- LeetCodeExercise/src/main/java/Main.java
+++ LeetCodeExercise/src/main/java/Main.java
@@ -3,9 +3,9 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        RemoveDuplicatesfromSortedArray r = new RemoveDuplicatesfromSortedArray();
-        int[] arr = {0,0,1,1,1,2,2,3,3,4};
-        System.out.println(r.removeDuplicates(arr));
+        RemoveElement r = new RemoveElement();
+        int[] arr = {0,1,2,2,3,0,4,2};
+        System.out.println(r.removeElement(arr, 2));
 
 
     }
